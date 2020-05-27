511032058e4ea234d25cde2f3c57393cc770941c
ChrisD921 <cdrape921@gmail.com>
2020-05-27 16:03:27 +0800

    Attempting Authentication and Applying UI

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,7 @@ Route::get('/borderLedger/profile', 'PersonnelController@profile');
 //     return view('ledger');
 // });
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
